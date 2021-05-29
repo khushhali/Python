a3ef8dc5e492f9544c119df389bcc74fe745ab31
khushhali <84490965+khushhali@users.noreply.github.com>
2021-05-29 22:29:07 +0530

    Add files via upload

---
+++ 5_bmiCalc.py
@@ -0,0 +1,17 @@
+'''
+ bmi calculator
+'''
+
+w=float(input('enter your weight='))
+h=float(input('enter your height='))
+print('weight w=',w,'height h=',h)
+print('weight w in (kg) ,','height h in (m)')
+bmi=w/(h**2)
+
+print('..............')
+
+
+
+print('enter the bmi of your body is:-,',bmi)
+
+    
