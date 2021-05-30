2a79a98a69ee9b7762ef820c39363724b84f3545
khushhali <84490965+khushhali@users.noreply.github.com>
2021-05-30 11:59:18 +0530

    Add files via upload

---
+++ 6_evenOdd.py
@@ -0,0 +1,10 @@
+
+'''
+check the given number is odd or even
+'''
+a=int(input('enter any number:',))
+remainder=a%2
+if(remainder==0):
+    print('your given number is even')
+else:
+    print('your given number is odd')
