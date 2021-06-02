7a70b4874e8520037f8e786099fbd3fa62cfd7ad
khushhali <84490965+khushhali@users.noreply.github.com>
2021-06-02 16:20:17 +0530

    Add files via upload

---
+++ 8_simpleInt.py
@@ -0,0 +1,20 @@
+
+ 
+'''
+simple int calculator
+'''
+'''
+principe amt, rate, time, intrest, total amount
+'''
+
+print('enter below details:-')
+pAmt = float(input('enter your principal amount:-'))
+r = float(input('enter your rate of interest(yearly):-'))
+t = float(input(' enter time period (in years):-'))
+
+intr = (pAmt*r*t)/100
+print('your total intrest is=',intr)
+tAmt = intr + pAmt
+print('your tottal amount is=',tAmt)
+
+
