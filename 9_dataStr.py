432c886c30089789056bb911dea6f4762c1f53ef
khushhali <84490965+khushhali@users.noreply.github.com>
2021-06-02 17:09:23 +0530

    Add files via upload

---
+++ 9_dataStr.py
@@ -0,0 +1,38 @@
+#python inbuilt data structure
+1_list
+2_tuple
+3_set
+4_dictionary
+
+#list:-
+            list is an orderd collection of elements .It is a mutable object ie;
+            it is possible to add new object or dlt any object from it. -
+           Ex:- [1,2,3,4,5,6]
+            *  it is created by placing elements in[]and seperated
+               by commas.
+            *  indexing is define for each value that is prsnt in
+               list.
+
+#tuple:-
+         tuple is an orderd collection of elements. it is a immutable object ie;
+         it is not possible to change the elements of the tuple.
+      Ex:-(1,2,3,4,5,6)
+      *  it is created by placing elements in() and seperated by commas.
+      * indexing is define for each value that is prsnt in tuple.
+
+#set:-
+      set is an unorderd collection of entities.it contains unique values or
+      entities.
+    ----- there is no indexing in set.
+    * in general set should be immutrable objest but it is an mutable object.
+    ## in set we can't give a name to the elements as a list but can give
+    name to the elements of the set as a tuple and set.
+    Ex:- {int,float ,tuple, string}
+
+#Dictionary:-
+               dict is an unordered collection of elements in python where data
+               is stored in key: value pairs.
+        Ex:-{a:2,b:3,c:4,d:5}
+       *  key should be mutable but value can be mutable or immutable both.
+        # dict is mutable object itself.
+             
